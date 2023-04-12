90cb050e630d3d909b752c3a148e6e240ce3e2ac
Dustin Graham <dustin@extend.com>
2023-04-12 15:47:44 -0600

    [PAR-3773] create initial options using option factory

--- Setup/Model/ProductInstaller.php
+++ Setup/Model/ProductInstaller.php
@@ -9,6 +9,7 @@ namespace Extend\Integration\Setup\Model;
 use Exception;
 use Extend\Integration\Service\Extend;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Api\ProductCustomOptionRepositoryInterface;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\Product\Gallery\EntryFactory;
 use Magento\Catalog\Model\Product\Gallery\GalleryManagement;
@@ -16,6 +17,8 @@ use Magento\Catalog\Model\Product\Type;
 use Magento\Catalog\Model\Product\Visibility;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\ProductFactory;
+use Magento\Catalog\Model\Product\Option;
+use Magento\Catalog\Model\Product\OptionFactory;
 use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
 use Magento\Eav\Api\Data\AttributeSetInterface;
 use Magento\Framework\Api\ImageContentFactory;
@@ -41,6 +44,9 @@ class ProductInstaller
     private Product $product;
     private ProductFactory $productFactory;
     private ProductRepositoryInterface $productRepository;
+    private Option $catalogOption;
+    private OptionFactory $catalogOptionFactory;
+    private ProductCustomOptionRepositoryInterface $optionRepository;
     private ProductResource $productResource;
     private Reader $reader;
     private StoreManagerInterface $storeManager;
@@ -54,6 +60,9 @@ class ProductInstaller
         ImageContentFactory $imageContentFactory,
         Product $product,
         ProductFactory $productFactory,
+        ProductCustomOptionRepositoryInterface $optionRepository,
+        Option $catalogOption,
+        OptionFactory $catalogOptionFactory,
         ProductRepositoryInterface $productRepository,
         ProductResource $productResource,
         Reader $reader,
@@ -67,6 +76,9 @@ class ProductInstaller
         $this->imageContentFactory = $imageContentFactory;
         $this->product = $product;
         $this->productFactory = $productFactory;
+        $this->catalogOption = $catalogOption;
+        $this->catalogOptionFactory = $catalogOptionFactory;
+        $this->optionRepository = $optionRepository;
         $this->productResource = $productResource;
         $this->storeManager = $storeManager;
         $this->productRepository = $productRepository;
@@ -80,6 +92,7 @@ class ProductInstaller
             if ($product = $this->createProtectionPlanProduct($attributeSet)) {
                 $this->addImageToPubMedia();
                 $this->processMediaGalleryEntry($this->getMediaImagePath(), $product->getSku());
+                $this->addOptionsToProtectionPlanProduct($product);
             }
         } catch (Exception $exception) {
             throw new Exception('There was an error creating the Extend Protection Plan Product' . $exception);
@@ -140,9 +153,8 @@ class ProductInstaller
                     'manage_stock' => 0,
                     'use_config_notify_stock_qty' => 0
                 ]);
-
+            
             $this->productRepository->save($product);
-
             return $product;
         } catch (Exception $exception) {
             throw new SetupException(
@@ -151,6 +163,78 @@ class ProductInstaller
                 )
             );
         }
+    }
+
+    private function addOptionsToProtectionPlanProduct(Product $product)
+    {
+        try {
+            $options = [
+                [
+                    'sort_order' => 0,
+                    'title'      => 'Associated Product',
+                    'type'       => 'field',
+                    'price_type' => 'fixed',
+                    'price'      => '0.00',
+                    'is_require' => 1,
+                ],
+                [
+                    'sort_order' => 0,
+                    'title'      => 'Plan Type',
+                    'type'       => 'field',
+                    'price_type' => 'fixed',
+                    'price'      => '0.00',
+                    'is_require' => 1,
+                ],
+                [
+                    'sort_order' => 0,
+                    'title'      => 'Plan ID',
+                    'type'       => 'field',
+                    'price_type' => 'fixed',
+                    'price'      => '0.00',
+                    'is_require' => 1,
+                ],
+                [
+                    'sort_order' => 0,
+                    'title'      => 'Term',
+                    'type'       => 'field',
+                    'price_type' => 'fixed',
+                    'price'      => '0.00',
+                    'is_require' => 1,
+                ],
+                [
+                    'sort_order' => 0,
+                    'title'      => 'List Price',
+                    'type'       => 'field',
+                    'price_type' => 'fixed',
+                    'price'      => '0.00',
+                    'is_require' => 0,
+                ],
+                [
+                    'sort_order' => 0,
+                    'title'      => 'Order Offer Plan Id',
+                    'type'       => 'field',
+                    'price_type' => 'fixed',
+                    'price'      => '0.00',
+                    'is_require' => 0,
+                ],
+            ];
+    
+            foreach ($options as $arrayOption) {
+                $option = $this->catalogOptionFactory->create();
+                $option->setProductId($product->getId())
+                    ->setStoreId($product->getStoreId())
+                    ->setProductSku($product->getSku())
+                    ->addData($arrayOption);
+                $this->optionRepository->save($option);
+            }
+        } catch (Exception $exception) {
+            throw new SetupException(
+                new Phrase('There was a problem adding the Extend Protection Product options: ',
+                    [$exception->getMessage()]
+                )
+            );
+        }
+
     }
 
     /**
