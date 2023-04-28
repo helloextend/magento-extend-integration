2c7cb3c86771ce74620726f0d182080a3add2a63
Scott Taylor <Scott.Taylor@Extend.com>
2023-04-28 14:58:27 -0400

    [PAR-2683] partial work on Product PP js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,11 @@
 var config = {
-  paths: {
-      extendSdk: 'https://sdk.helloextend.com/extend-sdk-client/v1/extend-sdk-client.min.js',
-      ExtendMagento: 'https://sdk.helloextend.com/extend-sdk-client-magento-addon/v1/extend-sdk-client-magento-addon.min.js'
-  }
+    paths: {
+        extendSdk: 'https://sdk.helloextend.com/extend-sdk-client/v1/extend-sdk-client.min',
+        ExtendMagento: 'https://sdk.helloextend.com/extend-sdk-client-magento-addon/v1/extend-sdk-client-magento-addon.min'
+    },
+    map: {
+        "*": {
+            productProtectionOffer: "Extend_Integration/js/view/catalog/product/product-protection-offer"
+        }
+    }
 }
---
+++ view/frontend/web/js/view/catalog/product/product-protection-offer.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright Extend (c) 2023. All rights reserved.
+ * See Extend-COPYING.txt for license details.
+ */
+
+define(
+    [
+        'jquery',
+        'extendSdk',
+        'ExtendMagento'
+    ],
+    function ($, Extend, ExtendMagento) {
+        'use strict';
+
+        var options = {};
+
+        return function(config, element) {
+            Extend.buttons.render('#product_protection_offer', {
+                referenceId: config.selectedProductSku,
+                price: config.selectedProductPrice,
+                category: config.productCategory
+            });
+        };
+
+    }
+)
