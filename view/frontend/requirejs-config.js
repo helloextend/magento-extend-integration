761ca7c9f056ab00fc2bebd2450be7fb6c6beb73
Miles McLeod <miles.mcleod@extend.com>
2023-05-03 13:23:45 -0700

    format js file showing up as changed in remote diff

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,13 @@
 var config = {
   paths: {
+<<<<<<< HEAD
       extendSdk: 'https://sdk.helloextend.com/extend-sdk-client/v1/extend-sdk-client.min',
       ExtendMagento: 'https://sdk.helloextend.com/extend-sdk-client-magento-addon/v1/extend-sdk-client-magento-addon.min'
   }
+=======
+    extendSdk: 'https://sdk.helloextend.com/extend-sdk-client/v1/extend-sdk-client.min.js',
+    ExtendMagento:
+      'https://sdk.helloextend.com/extend-sdk-client-magento-addon/v1/extend-sdk-client-magento-addon.min.js',
+  },
+>>>>>>> 48825c8 (format js file showing up as changed in remote diff)
 }
