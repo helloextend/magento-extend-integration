da6fc7e13c5589c187551fdef52b9b8a64d1d029
Miles McLeod <miles.mcleod@extend.com>
2023-05-05 10:04:36 -0700

    update linting to use custom xml linting config

--- scripts/lint-and-display-diff.sh
+++ scripts/lint-and-display-diff.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script is used to check the formatting of files with a specific file extension.
+
+# If the provided argument is an incompatible file extension, exit with a status code of 1
+if [ "$1" != "xml" ] && [ "$1" != "php" ]; then
+  echo "Invalid argument. Please specify 'xml' or 'php'."
+  exit 1
+fi
+
 # Get a list of files that have been added, copied, modified, renamed, or changed
 files=$(git diff --diff-filter=ACMRTUXB --name-only origin/master)
 
@@ -18,8 +26,15 @@ if ! git diff --quiet; then
   exit 1
 fi
 
+# Set the prettier command to a variable, based on file extension
+if [ "$1" == "xml" ]; then
+  prettier_command="yarn prettier --parser=$1 --config .prettierrc.xml.json"
+else
+  prettier_command="yarn prettier --parser=$1"
+fi
+
 # Check the formatting of the files
-npx prettier --parser=$1 --check $matched_files
+eval "$prettier_command --check $matched_files"
 
 # If there are no issues, exit with a status code of 0
 if [ $? -eq 0 ]; then
@@ -30,12 +45,12 @@ fi
 # Otherwise show a diff of the changes and exit with a status code of 1.
 # This runs prettier (altering the files), runs a git diff against origin/master,
 # and then restores the files to their original state.
-npx prettier --parser=$1 --write --loglevel silent $matched_files
+eval "$prettier_command --write --loglevel silent $matched_files"
 git --no-pager diff origin/master $matched_files
 git -C $(git rev-parse --show-toplevel) restore .
 
 echo -e "\n"
 echo -e "These ^ files will be auto-formatted upon commit... or you can:"
-echo -e "fix files individually: 'npx prettier --write \"<path-to-file>\"'"
-echo -e "fix all files: 'git diff --diff-filter=ACMRTUXB --name-only origin/master | grep -E \"(.$1$)\" | xargs npx prettier --parser=$1 --write'"
+echo -e "fix files individually: '$prettier_command --write \"<path-to-file>\"'"
+echo -e "fix all $1 files: 'git diff --diff-filter=ACMRTUXB --name-only origin/master | grep -E \"(.$1$)\" | xargs $prettier_command --parser=$1 --write'"
 exit 1
