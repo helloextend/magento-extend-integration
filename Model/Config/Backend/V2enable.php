f4f2bd5d9245eb489fa22afccd3a8e952ced7309
Joshua Thompson <joshua.thompson@extend.com>
2023-05-23 15:00:13 -0400

    Update comment

--- Model/Config/Backend/V2enable.php
+++ Model/Config/Backend/V2enable.php
@@ -45,7 +45,7 @@ class V2enable extends Value
     }
 
     /**
-     * This will disable Product Protection on the new module if it's enabled on the old module.
+     * This will create a new Product Protection product on the new module when V2 PP is enabled
      *
      * @return V2enable
      */
