570b89abba81b0e7741c8cefee0ae4b4dd7bfe56
Jay Faust <jay.faust@extend.com>
2023-05-24 09:07:34 -0400

    Further simplified button search logic

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -9,32 +9,9 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
   return function (config, element) {
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
 
-    // const offerDiv = $('#product_protection_offer_' + config[0].productSku)
-
-    // if (offerDiv) {
-    //   const actionsDiv = offerDiv.closest('.product.actions.product-item-actions')
-    //   if (actionsDiv) {
-    //     const primaryActionsDiv = actionsDiv.find('.actions-primary')
-    //     if (primaryActionsDiv) {
-    //       const addToCartButton = actionsDiv.find('.action.tocart.primary')
-    //       if (addToCartButton) {
-    //         console.log('addToCartButton found!')
-    //       } else {
-    //         console.log('addToCartButton not found')
-    //       }
-    //     } else {
-    //       console.log('primaryActionsDiv not found')
-    //     }
-    //   } else {
-    //     console.log('actionsDiv not found')
-    //   }
-    // } else {
-    //   console.log('offerDiv not found')
-    // }
-
     const addToCartButton = $('#product_protection_offer_' + config[0].productSku)
       .closest('.product.actions.product-item-actions')
-      .find('.actions-primary')
+      // .find('.actions-primary')
       .find('.action.tocart.primary')
 
     if (addToCartButton) {
