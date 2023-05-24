c0e6405a2d6e73e19ac5edbbb59473d43b7ce1a1
Jay Faust <jay.faust@extend.com>
2023-05-24 14:00:43 -0400

    Passed sku to modal instead of id

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -10,7 +10,9 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
     Extend.setDebug(true)
 
-    const addToCartButtonForm = $('#product_protection_modal_offer_' + config[0].productSku)
+    const productSku = config[0].productSku
+
+    const addToCartButtonForm = $('#product_protection_modal_offer_' + productSkuu)
       .closest('.product.actions.product-item-actions')
       .find('.actions-primary')
       .find('form[data-role="tocart-form"]')
@@ -20,10 +22,10 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
     if (addToCartButton) {
       addToCartButton.removeAttr('type').attr('type', 'button')
 
-      addToCartButton.click(function (event) {
+      addToCartButton.click(function () {
         Extend.modal
           .open({
-            referenceId: config[0].productId,
+            referenceId: productSku,
             price: config[0].productPrice * 100,
             category: config[0].productCategory,
             onClose: function (plan, product) {
