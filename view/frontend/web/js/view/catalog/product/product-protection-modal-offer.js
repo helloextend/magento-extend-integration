e48ac0e77b299d4adee1e74afdfabf3534ff92d7
Jay Faust <jay.faust@extend.com>
2023-05-24 08:48:33 -0400

    Updated traversal logic

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -10,23 +10,44 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
 
     const offerDiv = $('#product_protection_offer_' + config[0].productSku)
+    // if (offerDiv) {
+    //   const secondaryActionsDiv = offerDiv.closest('.actions-secondary')
+    //   if (secondaryActionsDiv) {
+    //     const primaryActionsDivs = secondaryActionsDiv.siblings('.actions-primary')
+    //     if (primaryActionsDivs.length) {
+    //       const primaryActionsDiv = primaryActionsDivs[0]
+    //       const addToCartButton = primaryActionsDiv.find('button')
+    //       if (addToCartButton) {
+    //         addToCartButton.html('<span>Modified</span>')
+    //       } else {
+    //         console.log('addToCartButton not found')
+    //       }
+    //     } else {
+    //       console.log('primaryActionsDivs not found')
+    //     }
+    //   } else {
+    //     console.log('secondaryActionsDiv not found')
+    //   }
+    // } else {
+    //   console.log('offerDiv not found')
+    // }
+
     if (offerDiv) {
-      const secondaryActionsDiv = offerDiv.closest('.actions-secondary')
-      if (secondaryActionsDiv) {
-        const primaryActionsDivs = secondaryActionsDiv.siblings('.actions-primary')
-        if (primaryActionsDivs.length) {
-          const primaryActionsDiv = primaryActionsDivs[0]
-          const addToCartButton = primaryActionsDiv.find('button')
+      const actionsDiv = offerDiv.closest('.product.actions.product-item-actions')
+      if (actionsDiv) {
+        const primaryActionsDiv = actionsDiv.find('.actions-primary')
+        if (primaryActionsDiv) {
+          const addToCartButton = actionsDiv.find('button')
           if (addToCartButton) {
             addToCartButton.html('<span>Modified</span>')
           } else {
             console.log('addToCartButton not found')
           }
         } else {
-          console.log('primaryActionsDivs not found')
+          console.log('primaryActionsDiv not found')
         }
       } else {
-        console.log('secondaryActionsDiv not found')
+        console.log('actionsDiv not found')
       }
     } else {
       console.log('offerDiv not found')
