3381ddc17c8f808b485560804f1e1c703e4045a4
Jay Faust <jay.faust@extend.com>
2023-05-24 09:02:51 -0400

    Cleaned up traversal logic

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -9,55 +9,39 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
   return function (config, element) {
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
 
-    const offerDiv = $('#product_protection_offer_' + config[0].productSku)
+    // const offerDiv = $('#product_protection_offer_' + config[0].productSku)
+
     // if (offerDiv) {
-    //   const secondaryActionsDiv = offerDiv.closest('.actions-secondary')
-    //   if (secondaryActionsDiv) {
-    //     const primaryActionsDivs = secondaryActionsDiv.siblings('.actions-primary')
-    //     if (primaryActionsDivs.length) {
-    //       const primaryActionsDiv = primaryActionsDivs[0]
-    //       const addToCartButton = primaryActionsDiv.find('button')
+    //   const actionsDiv = offerDiv.closest('.product.actions.product-item-actions')
+    //   if (actionsDiv) {
+    //     const primaryActionsDiv = actionsDiv.find('.actions-primary')
+    //     if (primaryActionsDiv) {
+    //       const addToCartButton = actionsDiv.find('.action.tocart.primary')
     //       if (addToCartButton) {
-    //         addToCartButton.html('<span>Modified</span>')
+    //         console.log('addToCartButton found!')
     //       } else {
     //         console.log('addToCartButton not found')
     //       }
     //     } else {
-    //       console.log('primaryActionsDivs not found')
+    //       console.log('primaryActionsDiv not found')
     //     }
     //   } else {
-    //     console.log('secondaryActionsDiv not found')
+    //     console.log('actionsDiv not found')
     //   }
     // } else {
     //   console.log('offerDiv not found')
     // }
 
-    if (offerDiv) {
-      const actionsDiv = offerDiv.closest('.product.actions.product-item-actions')
-      if (actionsDiv) {
-        const primaryActionsDiv = actionsDiv.find('.actions-primary')
-        if (primaryActionsDiv) {
-          const addToCartButton = actionsDiv.find('.action.tocart.primary')
-          if (addToCartButton) {
-            console.log('addToCartButton found!')
-          } else {
-            console.log('addToCartButton not found')
-          }
-        } else {
-          console.log('primaryActionsDiv not found')
-        }
-      } else {
-        console.log('actionsDiv not found')
-      }
-    } else {
-      console.log('offerDiv not found')
-    }
+    const addToCartButton = $('#product_protection_offer_' + config[0].productSku)
+      .closest('.product.actions.product-item-actions')
+      .find('.actions-primary')
+      .find('.action.tocart.primary')
 
-    // $('product_protection_modal_offer_' + config[0].productSku)
-    //   .closest('.actions-secondary')
-    //   .siblings('.actions-primary')
-    //   .find('button')
-    //   .html('<span>Modified</span>')
+    if (addToCartButton) {
+      console.log('addToCartButton found!')
+    } else {
+      console.log('addToCartButton not found')
+    }
 
     // Extend.modal.open({
     //   referenceId: config[0].productId,
