1e7f281c06d5d6856909eb661a0db167363e1ecf
Jay Faust <jay.faust@extend.com>
2023-05-24 09:24:46 -0400

    Attempting to add modal functionality

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -3,15 +3,11 @@
  * See Extend-COPYING.txt for license details.
  */
 
-define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMagento) {
+define(['extendSdk', 'ExtendMagento'], function (Extend, ExtendMagento) {
   'use strict'
 
   return function (config, element) {
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
-
-    // const addToCartButton = $('#product_protection_offer_' + config[0].productSku)
-    //   .closest('.product.actions.product-item-actions')
-    //   .find('.action.tocart.primary')
 
     const addToCartButton = document
       .getElementById('#product_protection_offer_' + config[0].productSku)
@@ -19,17 +15,18 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
       .querySelector('.action.tocart.primary')
 
     if (addToCartButton) {
-      console.log('addToCartButton found!')
+      addToCartButton.addEventListener('click', function () {
+        Extend.modal.open({
+          referenceId: config[0].productId,
+          price: config[0].productPrice * 100,
+          category: config[0].productCategory,
+          onClose: function (plan, product) {
+            console.log('onClose invoked', plan, product)
+          },
+        })
+      })
     } else {
       console.log('addToCartButton not found')
     }
-
-    // Extend.modal.open({
-    //   referenceId: config[0].productId,
-    //   price: config[0].productPrice * 100,
-    //   category: config[0].productCategory,
-
-    //   onClose: function (plan, product) {},
-    // })
   }
 })
