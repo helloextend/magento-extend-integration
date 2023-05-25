b351716e1af5f711354079eb7ef714c932c9798b
Jay Faust <jay.faust@extend.com>
2023-05-25 16:23:35 -0400

    Removed unneeded form event removal

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -18,10 +18,6 @@ define(['extendSdk', 'ExtendMagento'], function (Extend, ExtendMagento) {
       ?.querySelector('form[data-role="tocart-form"]')
 
     if (addToCartForm) {
-      addToCartForm.addEventListener('submit', function (event) {
-        event.preventDefault()
-      })
-
       const addToCartButton = addToCartForm.querySelector('.action.tocart.primary')
 
       if (addToCartButton) {
