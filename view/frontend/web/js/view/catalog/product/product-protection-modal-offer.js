56bacc4eeff6ce04cbbe05f2457727cde144dec4
Jay Faust <jay.faust@extend.com>
2023-05-25 15:49:24 -0400

    Trying w jQuery again 2 attempt minicart issue fix

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -3,7 +3,7 @@
  * See Extend-COPYING.txt for license details.
  */
 
-define(['extendSdk', 'ExtendMagento'], function (Extend, ExtendMagento) {
+define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMagento) {
   'use strict'
 
   return function (config, element) {
@@ -18,7 +18,7 @@ define(['extendSdk', 'ExtendMagento'], function (Extend, ExtendMagento) {
       ?.querySelector('form[data-role="tocart-form"]')
 
     if (addToCartForm) {
-      const addToCartButton = addToCartForm.querySelector('.action.tocart.primary')
+      const addToCartButton = addToCartForm.closest('.action.tocart.primary')
 
       if (addToCartButton) {
         addToCartButton.addEventListener('click', function (event) {
