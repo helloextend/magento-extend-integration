185e041e0eac85ca49c34b4d30c06b9f7fe7267b
Jomar Santos <jomar.santos@extend.com>
2023-05-26 15:19:05 -0700

    [PAR-4264] reload on success

--- view/frontend/web/js/view/cart/minicart-updates.js
+++ view/frontend/web/js/view/cart/minicart-updates.js
@@ -7,22 +7,28 @@ define(['jquery', 'uiComponent', 'Magento_Customer/js/customer-data', 'extendSdk
   'use strict'
 
   return Component.extend({
+    minicartSelector: '[data-block="minicart"]',
+    productItemSelector: '[data-role=product-item]',
+    itemDetailsSelector: 'div.product-item-details',
+    simpleOfferClass: 'extend-minicart-simple-offer',
+
     initialize: function () {
       this._super()
-      $('[data-block="minicart"]').on('contentUpdated', this.handleUpdate)
+      $(this.minicartSelector).on('contentUpdated', this.handleUpdate.bind(this))
     },
 
     handleUpdate: function () {
+      const self = this
       const cartItems = customerData.get('cart')().items
 
       cartItems.forEach(cartItem => {
         const qtyElem = document.getElementById(`cart-item-${cartItem.item_id}-qty`)
         if (qtyElem) {
-          const itemContainerElem = qtyElem.closest('[data-role=product-item]')
+          const itemContainerElem = qtyElem.closest(this.productItemSelector)
 
           if (itemContainerElem) {
-            const simpleOfferElemId = 'extend-minicart-simple-offer-' + cartItem.item_id
-            let simpleOfferElem = $('#' + simpleOfferElemId, itemContainerElem)
+            const simpleOfferElemId = `extend-minicart-simple-offer-${cartItem.item_id}`
+            let simpleOfferElem = $(`#${simpleOfferElemId}`, itemContainerElem)
 
             if (simpleOfferElem.length) {
               // TODO: If warranty already in cart, remove element
@@ -31,17 +37,16 @@ define(['jquery', 'uiComponent', 'Magento_Customer/js/customer-data', 'extendSdk
 
               simpleOfferElem = $('<div>')
                 .attr('id', simpleOfferElemId)
-                .addClass('extend-minicart-simple-offer')
-              const itemDetailsElem = $('div.product-item-details', itemContainerElem)
+                .addClass(this.simpleOfferClass)
+              const itemDetailsElem = $(this.itemDetailsSelector, itemContainerElem)
 
               if (itemDetailsElem.length) {
                 itemDetailsElem.append(simpleOfferElem)
                 Extend.buttons.renderSimpleOffer(`#${simpleOfferElemId}`, {
                   referenceId: cartItem.product_sku,
                   price: cartItem.product_price_value * 100,
-                  // category: cartItem.category,
                   onAddToCart: function (opts) {
-                    console.log('onAddToCart invoked', opts)
+                    self.addToCart(opts)
                   },
                 })
               }
@@ -49,6 +54,16 @@ define(['jquery', 'uiComponent', 'Magento_Customer/js/customer-data', 'extendSdk
           }
         }
       })
+    },
+
+    addToCart: function (opts) {
+      // TODO: Handle adding to cart
+      console.log('addToCart', opts)
+      this.addToCartSuccess()
+    },
+
+    addToCartSuccess: function () {
+      customerData.reload(['cart'], false)
     },
   })
 })
