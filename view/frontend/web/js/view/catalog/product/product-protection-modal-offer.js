3074854a185d4bae12a92c0312792f1163c71fd7
Jay Faust <jay.faust@extend.com>
2023-05-26 10:41:56 -0400

    Removed all references to form element

--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -11,32 +11,25 @@ define(['extendSdk', 'ExtendMagento'], function (Extend, ExtendMagento) {
 
     const productSku = config[0].productSku
 
-    const addToCartForm = document
+    const addToCartButton = document
       .querySelector('#product_protection_modal_offer_' + productSku)
       ?.closest('.product.actions.product-item-actions')
       ?.querySelector('.actions-primary')
       ?.querySelector('form[data-role="tocart-form"]')
+      ?.querySelector('.action.tocart.primary')
 
-    if (addToCartForm) {
-      const addToCartButton = addToCartForm.querySelector('.action.tocart.primary')
-
-      if (addToCartButton) {
-        addToCartButton.addEventListener('click', function (event) {
-          // this button is of type submit so clicking it automatically submits the form, the form now gets submitted on modal close
-          // event.preventDefault()
-
-          Extend.modal.open({
-            referenceId: productSku,
-            price: config[0].productPrice * 100,
-            category: config[0].productCategory,
-            onClose: function (plan, product) {
-              // TODO: [PAR-4187] Add add to cart functionality
-              console.log('onClose invoked', { plan }, { product })
-              // addToCartForm.submit()
-            },
-          })
+    if (addToCartButton) {
+      addToCartButton.addEventListener('click', function () {
+        Extend.modal.open({
+          referenceId: productSku,
+          price: config[0].productPrice * 100,
+          category: config[0].productCategory,
+          onClose: function (plan, product) {
+            // TODO: [PAR-4187] Add add to cart functionality
+            console.log('onClose invoked', { plan }, { product })
+          },
         })
-      }
+      })
     }
   }
 })
