fa34582427335f1f0781b1b96222a45dd6e1e4fe
Scott Taylor <Scott.Taylor@Extend.com>
2023-05-30 16:51:46 -0400

    [PAR-4186] PDP modal offer

--- view/frontend/web/js/view/catalog/product/product-protection-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-offer.js
@@ -8,7 +8,7 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
 
   function getActiveProductConfig() {
     const swatches = $('div.swatch-attribute', '.product-info-main')
-    let selectedSku = null
+    let selectedProductSku = null
     let selectedPrice = null
 
     if (swatches.length > 0) {
@@ -22,17 +22,17 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
         if (selectedId && selectedId !== '') {
           selectedPrice =
             swatchRenderer.options.jsonConfig.optionPrices[selectedId].finalPrice.amount
-          selectedSku = swatchRenderer.options.jsonConfig.skus[selectedId]
+          selectedProductSku = swatchRenderer.options.jsonConfig.skus[selectedId]
         }
       }
-    } else if (this.options.isInProductView) {
+    } else {
       const spConfig = $('#product_addtocart_form').data('mageConfigurable').options.spConfig
       const selectedId = $('input[name=selected_configurable_option]', '.product-info-main').val()
       if (selectedId && selectedId !== '') {
-        selectedSku = spConfig && spConfig.skus ? spConfig.skus[selectedId] : null
+        selectedProductSku = spConfig && spConfig.skus ? spConfig.skus[selectedId] : null
       }
     }
-    return { selectedSku, selectedPrice }
+    return { selectedProductSku, selectedPrice }
   }
 
   return function (config, element) {
@@ -51,7 +51,7 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
         '#product_protection_offer_' + config[0].selectedProductSku,
       )
       const activeProductData = {
-        referenceId: selectedProduct.selectedSku,
+        referenceId: selectedProduct.selectedProductSku,
         price: selectedProduct.selectedPrice * 100,
         category: config.productCategory,
       }
@@ -62,6 +62,30 @@ define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMage
           '#product_protection_offer_' + config[0].selectedProductSku,
           activeProductData,
         )
+      }
+    })
+
+    document.getElementById('product-addtocart-button').addEventListener('click', function () {
+      let selectedProduct
+      const buttonInstance = Extend.buttons.instance(
+        '#product_protection_offer_' + config[0].selectedProductSku,
+      )
+      const plan = buttonInstance.getPlanSelection()
+      if (!plan && config.length === 1) {
+        if (buttonInstance.getActiveProduct().id === config[0].selectedProductSku) {
+          selectedProduct = config[0]
+        } else {
+          selectedProduct = getActiveProductConfig()
+        }
+        Extend.modal.open({
+          referenceId: selectedProduct.selectedProductSku,
+          price: selectedProduct.selectedPrice * 100,
+          category: config.productCategory,
+          onClose: function (plan, product) {
+            // TODO: [PAR-4187] Add add to cart functionality
+            console.log('onClose invoked', { plan }, { product })
+          },
+        })
       }
     })
   }
