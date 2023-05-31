75f676fa62166d1e7a726fea454ea8a3febf5a78
Jay Faust <jay.faust@extend.com>
2023-05-31 10:22:29 -0400

    Added add to cart functionality from simple offer

--- view/frontend/web/js/view/cart/product-protection-simple-offer.js
+++ view/frontend/web/js/view/cart/product-protection-simple-offer.js
@@ -3,17 +3,69 @@
  * See Extend-COPYING.txt for license details.
  */
 
-define(['jquery', 'extendSdk', 'ExtendMagento'], function ($, Extend, ExtendMagento) {
+define(['Magento_Customer/js/customer-data', 'extendSdk', 'ExtendMagento'], function (
+  customerData,
+  Extend,
+  ExtendMagento,
+) {
   'use strict'
+
+  const getCartItems = function () {
+    const cartItems = customerData
+      .get('cart')()
+      .items?.map(item => {
+        return {
+          name: item.product_name,
+          sku: item.product_sku,
+          qty: item.qty,
+          price: item.product_price_value * 100,
+          item_id: item.product_id,
+          options: [],
+        }
+      })
+
+    return cartItems ?? []
+  }
+
+  const refreshCart = function () {
+    const sectionsToUpdate = ['cart']
+    customerData.invalidate(sectionsToUpdate)
+    customerData.reload(sectionsToUpdate, true)
+  }
+
+  const handleAddToCartClick = function (opts) {
+    const { plan, product, quantity } = opts
+
+    if (plan && product) {
+      const { planId, price, term, title, coverageType, offerId } = plan
+      const { id: productId, price: listPrice } = product
+
+      const planToUpsert = {
+        planId,
+        price,
+        term,
+        title,
+        coverageType,
+      }
+      const cartItems = getCartItems()
+
+      ExtendMagento.upsertProductProtection({
+        plan: planToUpsert,
+        cartItems,
+        productId,
+        listPrice,
+        offerId,
+        quantity: quantity ?? 1,
+      }).then(refreshCart)
+    }
+  }
 
   return function (config, element) {
     const activeProductData = {
       referenceId: config[0].selectedProductSku,
       price: config[0].selectedProductPrice * 100,
       category: config[0].productCategory,
-      onAddToCart: function (opts) {
-        console.log('onAddToCart invoked', opts)
-      },
+      onAddToCart: handleAddToCartClick,
     }
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
     Extend.buttons.renderSimpleOffer(
--- view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
+++ view/frontend/web/js/view/catalog/product/product-protection-modal-offer.js
@@ -10,10 +10,67 @@ define(['Magento_Customer/js/customer-data', 'extendSdk', 'ExtendMagento'], func
 ) {
   'use strict'
 
+  const getCartItems = function () {
+    const cartItems = customerData
+      .get('cart')()
+      .items?.map(item => {
+        return {
+          name: item.product_name,
+          sku: item.product_sku,
+          qty: item.qty,
+          price: item.product_price_value * 100,
+          item_id: item.product_id,
+          options: [],
+        }
+      })
+
+    return cartItems ?? []
+  }
+
+  const refreshCart = function () {
+    const sectionsToUpdate = ['cart']
+    customerData.invalidate(sectionsToUpdate)
+    customerData.reload(sectionsToUpdate, true)
+  }
+
+  const handleAddToCartClick = function (productSku, productPrice, productCategory) {
+    Extend.modal.open({
+      referenceId: productSku,
+      price: productPrice,
+      category: productCategory,
+      onClose: function (plan, product) {
+        if (plan && product) {
+          const { planId, price, term, title, coverageType, offerId } = plan
+          const { id: productId, price: listPrice } = product
+
+          const planToUpsert = {
+            planId,
+            price,
+            term,
+            title,
+            coverageType,
+          }
+          const cartItems = getCartItems()
+
+          ExtendMagento.upsertProductProtection({
+            plan: planToUpsert,
+            cartItems,
+            productId,
+            listPrice,
+            offerId,
+            quantity: 1,
+          }).then(refreshCart)
+        }
+      },
+    })
+  }
+
   return function (config, element) {
     Extend.config({ storeId: config[0].extendStoreUuid, environment: config[0].activeEnvironment })
 
     const productSku = config[0].productSku
+    const productPrice = config[0].productPrice * 100
+    const productCategory = config[0].productCategory
 
     const addToCartButton = document
       .querySelector('#product_protection_modal_offer_' + productSku)
@@ -22,55 +79,11 @@ define(['Magento_Customer/js/customer-data', 'extendSdk', 'ExtendMagento'], func
       ?.querySelector('.action.tocart.primary')
 
     if (addToCartButton) {
-      addToCartButton.addEventListener('click', function () {
-        Extend.modal.open({
-          referenceId: productSku,
-          price: config[0].productPrice * 100,
-          category: config[0].productCategory,
-          onClose: function (plan, product) {
-            if (plan && product) {
-              const { planId, price, term, title, coverageType, offerId } = plan
-              const { id: productId, price: listPrice } = product
-              const magentoCartItems = customerData.get('cart')().items
-              console.log(
-                'modal closed, available data:',
-                { plan },
-                { product },
-                { magentoCartItems },
-              )
-              const cartItems = magentoCartItems.map(item => {
-                return {
-                  name: item.product_name,
-                  sku: item.product_sku,
-                  qty: item.qty,
-                  price: item.product_price_value * 100,
-                  item_id: item.product_id,
-                  options: [],
-                }
-              })
+      const handler = function () {
+        handleAddToCartClick(productSku, productPrice, productCategory)
+      }
 
-              ExtendMagento.upsertProductProtection({
-                plan: {
-                  planId,
-                  price,
-                  term,
-                  title,
-                  coverageType,
-                },
-                cartItems,
-                productId,
-                listPrice,
-                offerId,
-                quantity: 1,
-              }).then(() => {
-                const sectionsToUpdate = ['cart']
-                customerData.invalidate(sectionsToUpdate)
-                customerData.reload(sectionsToUpdate, true)
-              })
-            }
-          },
-        })
-      })
+      addToCartButton.addEventListener('click', handler)
     }
   }
 })
