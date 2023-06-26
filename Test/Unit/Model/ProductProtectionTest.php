95d1b3794d37ee5f48735b6c28407cc35f8cddee
Scott Taylor <Scott.Taylor@Extend.com>
2023-06-26 16:01:34 -0400

    [PAR-3232] partial unit test

--- Test/Unit/Model/ProductProtectionTest.php
+++ Test/Unit/Model/ProductProtectionTest.php
@@ -6,63 +6,97 @@
 
 namespace Extend\Integration\Test\Unit\Model;
 
+use Extend\Integration\Model\ProductProtection;
+use Extend\Integration\Service\Api\Integration;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Checkout\Model\Session;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Model\Quote\Item\OptionFactory;
+use Magento\Quote\Model\Quote\ItemFactory;
+use Magento\Setup\Exception;
 use Magento\Store\Model\StoreManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 
 class ProductProtectionTest extends TestCase
 {
+    /**
+     * @var CartRepositoryInterface|MockObject
+     */
+    private CartRepositoryInterface $quoteRepository;
+
     protected function setUp(): void
     {
-        $this->quoteRespository = $this->getMockBuilder(
-            \Magento\Quote\Api\CartRepositoryInterface::class
-        )
-            ->onlyMethods('save')
+        $this->quoteRespository = $this->getMockBuilder(CartRepositoryInterface::class)
+            ->onlyMethods(['save'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-        $this->ProductRepository = $this->getMockBuilder(ProductRepositoryInterface::class)
-            ->onlyMethods('get')
+        $this->productRepository = $this->getMockBuilder(ProductRepositoryInterface::class)
+            ->onlyMethods(['get'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-        $this->itemFactory = $this->getMockBuilder(\Magento\Quote\Model\Quote\ItemFactory::class)
-            ->onlyMethods('create')
+        $this->itemFactory = $this->getMockBuilder(ItemFactory::class)
+            ->onlyMethods(['create'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-        $this->itemOptionFactory = $this->getMockBuilder(
-            \Magento\Quote\Model\Quote\Item\OptionFactory::class
-        )
-            ->onlyMethods('create')
+        $this->itemOptionFactory = $this->getMockBuilder(OptionFactory::class)
+            ->onlyMethods(['create'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-        $this->checkoutSession = $this->getMockBuilder(\Magento\Checkout\Model\Session::class)
-            ->onlyMethods('getQuote')
+        $this->checkoutSession = $this->getMockBuilder(Session::class)
+            ->onlyMethods(['getQuote'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-        $this->integration = $this->getMockBuilder(
-            \Extend\Integration\Service\Api\Integration::class
-        )
-            ->onlyMethods('logErrorToLoggingService')
+        $this->integration = $this->getMockBuilder(Integration::class)
+            ->onlyMethods(['logErrorToLoggingService'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
         $this->storeManager = $this->getMockBuilder(StoreManagerInterface::class)
-            ->onlyMethods('getStore')
+            ->onlyMethods(['getStore'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
         $this->logger = $this->getMockBuilder(LoggerInterface::class)
-            ->onlyMethods('error')
+            ->onlyMethods(['error'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
+
+        $this->productProtection = new ProductProtection(
+            $this->quoteRespository,
+            $this->productRepository,
+            $this->itemFactory,
+            $this->itemOptionFactory,
+            $this->checkoutSession,
+            $this->integration,
+            $this->storeManager,
+            $this->logger
+        );
+
+        $this->upsertParameters = [
+            'quantity' => 2,
+            'cartItemId' => 289,
+            'productId' => 314,
+            'planId' => '038e65ca-5d3d-4ddf-a294-1b12e2dd8781',
+            'price' => 1.25,
+            'term' => 24,
+            'coverageType' => 'base',
+            'leadToken' => '4d172caf-d12d-40e2-bbf9-661af6337823',
+            'listPrice' => 32.45,
+            'orderOfferPlanId' => '86fb407c-0940-4a1b-ae23-dc98c3f1a235',
+        ];
     }
 
-    public function testUpsert()
+    public function testUpsertWhenPriceIsZero()
     {
+        $this->productProtection
+            ->upsert(explode(',', array_values($this->upsertParameters)))
+            ->willThrowException(new Exception());
     }
 }
