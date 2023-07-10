fc436e9ad33ddd001c750267e6b26a5535c8e59a
Scott Taylor <Scott.Taylor@Extend.com>
2023-07-10 11:18:15 -0400

    [PAR-4459] fix bug in observer plugin

--- Plugin/Event/ConfigPlugin.php
+++ Plugin/Event/ConfigPlugin.php
@@ -7,9 +7,28 @@
 namespace Extend\Integration\Plugin\Event;
 
 use Extend\Integration\Service\Extend;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Event\Config;
 
 class ConfigPlugin
 {
+    private ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Turns off observer calls if Extend Module turned off in admin
+     *
+     * @param Config $subject
+     * @param $result
+     * @return mixed
+     */
     public function afterGetObservers(\Magento\Framework\Event\Config $subject, $result)
     {
         if ($result) {
