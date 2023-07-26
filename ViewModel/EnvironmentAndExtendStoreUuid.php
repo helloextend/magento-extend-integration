1d039fb73f202ee3000e2d2f7e6bc2ec437ab1db
Miles McLeod <miles.mcleod@extend.com>
2023-07-26 09:11:59 -0700

    update cart normalization settings to be multi-scoped

--- ViewModel/EnvironmentAndExtendStoreUuid.php
+++ ViewModel/EnvironmentAndExtendStoreUuid.php
@@ -94,7 +94,7 @@ class EnvironmentAndExtendStoreUuid implements
 
     public function isCartBalancingEnabled(): bool
     {
-        return $this->scopeConfig->getValue(Extend::ENABLE_CART_BALANCING) === '1';
+        return $this->getScopedConfigValue(Extend::ENABLE_CART_BALANCING) === '1';
     }
 
     public function isProductProtectionProductDisplayPageOfferEnabled(): bool
