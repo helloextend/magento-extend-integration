a379218877876eee241a8e4ba0beb724373664be
Miles McLeod <miles.mcleod@extend.com>
2023-09-13 11:37:44 -0700

    refactor: [PAR-5177] restore old plugins and remove abstract db plugin (#46)
    * refactor: [PAR-5177] restore old plugins and remove abstract db plugin
    
    * auto-changelog
    
    * auto-changelog
    
    * auto-changelog
    
    * auto-changelog
    
    * auto-changelog
    
    * reformat php files to include 4 space tab size
    
    * auto-changelog
    
    * remove unnecessary fully-qualified names
    
    * auto-changelog
    
    * auto-changelog
    
    * auto-changelog
    
    ---------
    
    Co-authored-by: magento-bot <magento-bot@extend.com>

--- Plugin/Model/Order/CreditmemoRepositoryPlugin.php
+++ Plugin/Model/Order/CreditmemoRepositoryPlugin.php
@@ -6,29 +6,73 @@
 
 namespace Extend\Integration\Plugin\Model\Order;
 
+use Extend\Integration\Api\Data\ShippingProtectionTotalInterface;
+use Extend\Integration\Api\ShippingProtectionTotalRepositoryInterface;
 use Extend\Integration\Service\Api\Integration;
 use Extend\Integration\Service\Api\OrderObserverHandler;
-use Extend\Integration\Service\Extend;
+use Magento\Sales\Api\Data\CreditmemoExtensionFactory;
 use Magento\Sales\Model\Order\CreditmemoRepository;
+use Extend\Integration\Model\ShippingProtectionFactory;
+use Extend\Integration\Service\Extend;
 
 class CreditmemoRepositoryPlugin
 {
     /**
-     * @var OrderObserverHandler
+     * @var ShippingProtectionTotalRepositoryInterface
      */
+    private ShippingProtectionTotalRepositoryInterface $shippingProtectionTotalRepository;
+
+    /**
+     * @var CreditmemoExtensionFactory
+     */
+    private CreditmemoExtensionFactory $creditmemoExtensionFactory;
+    private ShippingProtectionFactory $shippingProtectionFactory;
     private OrderObserverHandler $orderObserverHandler;
+
     private Extend $extend;
 
     public function __construct(
+        ShippingProtectionTotalRepositoryInterface $shippingProtectionTotalRepository,
+        CreditmemoExtensionFactory $creditmemoExtensionFactory,
+        ShippingProtectionFactory $shippingProtectionFactory,
         OrderObserverHandler $orderObserverHandler,
         Extend $extend
     ) {
+        $this->shippingProtectionTotalRepository = $shippingProtectionTotalRepository;
+        $this->creditmemoExtensionFactory = $creditmemoExtensionFactory;
+        $this->shippingProtectionFactory = $shippingProtectionFactory;
         $this->orderObserverHandler = $orderObserverHandler;
         $this->extend = $extend;
     }
 
     /**
-     * This calls the Extend API endpoint to cancel the full or partial order at Extend.
+     * This plugin injects the Shipping Protection record into the credit memo's extension attributes if a matching record is found with a given credit memo ID
+     *
+     * @param CreditmemoRepository $subject
+     * @param $result
+     * @param $creditMemoId
+     * @return mixed
+     */
+    public function afterGet(
+        CreditmemoRepository $subject,
+        $result,
+        $creditMemoId
+    ) {
+        if (!$this->extend->isEnabled()) {
+            return $result;
+        }
+
+        $this->shippingProtectionTotalRepository->getAndSaturateExtensionAttributes(
+            $creditMemoId,
+            ShippingProtectionTotalInterface::CREDITMEMO_ENTITY_TYPE_ID,
+            $result
+        );
+
+        return $result;
+    }
+
+    /**
+     * This save the Shipping Protection data from the credit memo's extension attributes into the Shipping Protection totals table, saving the entity type and credit memo ID as well
      *
      * @param CreditmemoRepository $subject
      * @param $result
@@ -36,22 +80,35 @@ class CreditmemoRepositoryPlugin
      * @return mixed
      */
     public function afterSave(
-        \Magento\Sales\Model\Order\CreditmemoRepository $subject,
+        CreditmemoRepository $subject,
         $result,
         $creditMemo
     ) {
-        if (!$this->extend->isEnabled())
+        if (!$this->extend->isEnabled()) {
             return $result;
+        }
 
+        $extensionAttributes = $creditMemo->getExtensionAttributes();
+        if ($extensionAttributes === null) {
+            $extensionAttributes = $this->creditmemoExtensionFactory->create();
+        }
+        $shippingProtection = $extensionAttributes->getShippingProtection();
+
+        if ($result && $shippingProtection) {
+            $this->shippingProtectionTotalRepository->saveAndResaturateExtensionAttribute(
+                $shippingProtection,
+                $result,
+                ShippingProtectionTotalInterface::CREDITMEMO_ENTITY_TYPE_ID
+            );
+        }
         $this->orderObserverHandler->execute(
             [
-                'path' => Integration::EXTEND_INTEGRATION_ENDPOINTS['webhooks_orders_cancel'],
-                'type' => 'middleware',
+            'path' => Integration::EXTEND_INTEGRATION_ENDPOINTS['webhooks_orders_cancel'],
+            'type' => 'middleware',
             ],
             $result->getOrder(),
             ['credit_memo_id' => $creditMemo->getId()]
         );
-
         return $result;
     }
 }
