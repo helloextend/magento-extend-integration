a02b3a07393d389a1a03764a2d2efec2f8f0bd3e
Romel Dris <romel.dris@gmail.com>
2024-03-04 09:55:09 -0800

    feat: [MINT-1671] Additional metadata headers (#92)
    * feat: [MINT-1671] Additional metadata headers
    
    * auto-changelog
    
    ---------
    
    Co-authored-by: magento-bot <magento-bot@extend.com>

--- Service/Api/MetadataBuilder.php
+++ Service/Api/MetadataBuilder.php
@@ -6,11 +6,19 @@
 
 namespace Extend\Integration\Service\Api;
 
+use Exception;
 use Extend\Integration\Api\StoreIntegrationRepositoryInterface;
 use Magento\Framework\DataObject\IdentityService;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\App\ProductMetadataInterface;
 use Extend\Integration\Service\Api\AccessTokenBuilder;
+use Magento\Framework\Composer\ComposerInformation;
+use Magento\Integration\Api\IntegrationServiceInterface;
+use Magento\Integration\Api\OauthServiceInterface;
+use Extend\Integration\Service\Api\Integration;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Psr\Log\LoggerInterface;
+
 
 class MetadataBuilder
 {
@@ -18,17 +26,32 @@ class MetadataBuilder
     private StoreIntegrationRepositoryInterface $storeIntegrationRepository;
     private ProductMetadataInterface $productMetadata;
     private AccessTokenBuilder $accessTokenBuilder;
+    private ComposerInformation $composerInformation;
+    private IntegrationServiceInterface $integrationService;
+    private OauthServiceInterface $oauthService;
+    private ScopeConfigInterface $scopeConfig;
+    private LoggerInterface $logger;
 
     public function __construct(
         IdentityService $identityService,
         StoreIntegrationRepositoryInterface $storeIntegrationRepository,
         ProductMetadataInterface $productMetadata,
-        AccessTokenBuilder $accessTokenBuilder
+        AccessTokenBuilder $accessTokenBuilder,
+        ComposerInformation $composerInformation,
+        IntegrationServiceInterface $integrationService,
+        OauthServiceInterface $oauthService,
+        ScopeConfigInterface $scopeConfig,
+        LoggerInterface $logger
     ) {
         $this->identityService = $identityService;
         $this->storeIntegrationRepository = $storeIntegrationRepository;
         $this->productMetadata = $productMetadata;
         $this->accessTokenBuilder = $accessTokenBuilder;
+        $this->composerInformation = $composerInformation;
+        $this->oauthService = $oauthService;
+        $this->integrationService = $integrationService;
+        $this->scopeConfig = $scopeConfig;
+        $this->logger = $logger;
     }
 
     /**
@@ -48,6 +71,30 @@ class MetadataBuilder
         $fullMagentoVersion = $this->productMetadata->getVersion();
         $trimmedMagentoVersion = strstr($fullMagentoVersion, '-', true);
         $headers['X-Magento-Version'] = !$trimmedMagentoVersion ? $fullMagentoVersion : $trimmedMagentoVersion;
+
+        try {
+            $installedMagentoPackages = $this->composerInformation->getInstalledMagentoPackages();
+
+            if (!empty($installedMagentoPackages['helloextend/integration']['version'])) {
+                $headers['X-Extend-Mage-Module-Version'] = ($installedMagentoPackages['helloextend/integration']['version']);
+            }
+
+            $activeIntegration = $this->scopeConfig->getValue(Integration::INTEGRATION_ENVIRONMENT_CONFIG);
+            $integration = $this->integrationService->get($activeIntegration);
+            $oauth = $this->oauthService->loadConsumer($integration->getConsumerId());
+
+            $headers['X-Extend-Mage-Consumer-Key'] = $oauth->getKey();
+            // We are only including the storeIds in the header when there is only one store, otherwise this list may get too long
+            if (count($storeIds) === 1) {
+                $headers['X-Extend-Mage-Store-UUID'] = $this->storeIntegrationRepository
+                    ->getByStoreIdAndActiveEnvironment($storeIds[0])
+                    ->getStoreUuid();
+            }
+        } catch (Exception $exception) {
+            // silently fails
+            $this->logger->error('The follow error was reported while trying to add additional metadata headers: ' . $exception->getMessage());
+        }
+
 
         $body['webhook_id'] = $this->identityService->generateId();
         $body['webhook_created_at'] = time();
--- Test/Unit/Service/Api/MetadataBuilderTest.php
+++ Test/Unit/Service/Api/MetadataBuilderTest.php
@@ -14,6 +14,11 @@ use Extend\Integration\Api\Data\StoreIntegrationInterface;
 use Extend\Integration\Service\Api\MetadataBuilder;
 use Magento\Framework\App\ProductMetadataInterface;
 use Extend\Integration\Service\Api\AccessTokenBuilder;
+use \Magento\Framework\Composer\ComposerInformation;
+use Magento\Integration\Api\IntegrationServiceInterface;
+use Magento\Integration\Api\OauthServiceInterface;
+use Magento\Integration\Model\Oauth\Consumer;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class MetadataBuilderTest extends TestCase
 {
@@ -22,12 +27,20 @@ class MetadataBuilderTest extends TestCase
     private StoreIntegrationRepositoryInterface $storeIntegrationRepository;
     private ProductMetadataInterface $productMetadata;
     private AccessTokenBuilder $accessTokenBuilder;
+    private ComposerInformation $composerInformation;
+    private IntegrationServiceInterface $integrationService;
+    private OauthServiceInterface $oauthService;
+    private ScopeConfigInterface $scopeConfig;
+    private LoggerInterface $logger;
     private ObjectManager $objectManager;
     private MetadataBuilder $metadataBuilder;
     private array $magentoStoreIdMocks = [1];
     private string $generatedUUIDMock = 'acff4bd1-889c-431f-908e-24fea292337c';
     private string $magentoVersion = '2.4.2';
     private string $extendAccessToken = 'token';
+    private string $consumerKey = 'consumerKey';
+    private string $storeUUID = 'abf89f21-d240-477a-a462-14797ec5264e';
+    private string $moduleVersion = '1.0.0';
 
     protected function setUp(): void
     {
@@ -44,6 +57,11 @@ class MetadataBuilderTest extends TestCase
             ->onlyMethods(['getStoreUuid'])
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
+        $this->storeIntegration
+            ->expects($this->any())
+            ->method('getStoreUuid')
+            ->willReturn($this->storeUUID);
+
         $this->storeIntegrationRepository = $this->getMockBuilder(
             StoreIntegrationRepositoryInterface::class
         )
@@ -65,6 +83,44 @@ class MetadataBuilderTest extends TestCase
             ->method('getAccessToken')
             ->willReturn($this->extendAccessToken);
 
+        $this->composerInformation = $this->getMockBuilder(ComposerInformation::class)
+            ->onlyMethods(['getInstalledMagentoPackages'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $this->composerInformation
+            ->expects($this->any())
+            ->method('getInstalledMagentoPackages')
+            ->willReturn(['helloextend/integration' => ['version' => $this->moduleVersion]]);
+
+        $activeIntegration = 1;
+
+        $this->scopeConfig = $this->createMock(ScopeConfigInterface::class);
+        $this->scopeConfig->method('getValue')->willReturn($activeIntegration);
+
+        $this->integrationService = $this->createMock(IntegrationServiceInterface::class);
+        $integrationModel = $this->createMock(
+            \Magento\Integration\Model\Integration::class
+        );
+        $this->integrationService->method('get')->with($activeIntegration)->willReturn($integrationModel);
+
+        $mockConsumer = $this->getMockBuilder(Consumer::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getKey'])
+            ->getMock();
+        $mockConsumer
+            ->expects($this->once())
+            ->method('getKey')
+            ->willReturn($this->consumerKey);
+
+        $this->oauthService = $this->createMock(OauthServiceInterface::class);
+        $this->oauthService->method('loadConsumer')->willReturn($mockConsumer);
+
+        $this->oauthService
+            ->expects($this->any())
+            ->method('loadConsumer')
+            ->willReturn(['helloextend/integration' => $this->magentoVersion]);
+
         $this->productMetadata = $this->getMockBuilder(ProductMetadataInterface::class)
             ->onlyMethods(['getVersion'])
             ->disableOriginalConstructor()
@@ -80,6 +136,10 @@ class MetadataBuilderTest extends TestCase
             'storeIntegrationRepository' => $this->storeIntegrationRepository,
             'productMetadata' => $this->productMetadata,
             'accessTokenBuilder' => $this->accessTokenBuilder,
+            'composerInformation' => $this->composerInformation,
+            'oauthService' => $this->oauthService,
+            'integrationService' => $this->integrationService,
+            'scopeConfig' => $this->scopeConfig,
         ]);
     }
 
@@ -97,6 +157,9 @@ class MetadataBuilderTest extends TestCase
             'X-Extend-Access-Token' => $this->extendAccessToken,
             'Content-Type' => 'application/json',
             'X-Magento-Version' => $this->magentoVersion,
+            'X-Extend-Mage-Consumer-Key' => $this->consumerKey,
+            'X-Extend-Mage-Store-UUID' => $this->storeUUID,
+            'X-Extend-Mage-Module-Version' => $this->moduleVersion,
         ];
         $expectedBody = [
             'webhook_id' => $this->generatedUUIDMock,
@@ -118,6 +181,19 @@ class MetadataBuilderTest extends TestCase
         $this->assertEquals(
             $expectedHeaders['X-Magento-Version'],
             $actualHeaders['X-Magento-Version']
+        );
+
+        $this->assertEquals(
+            $expectedHeaders['X-Extend-Mage-Consumer-Key'],
+            $actualHeaders['X-Extend-Mage-Consumer-Key']
+        );
+        $this->assertEquals(
+            $expectedHeaders['X-Extend-Mage-Store-UUID'],
+            $actualHeaders['X-Extend-Mage-Store-UUID']
+        );
+        $this->assertEquals(
+            $expectedHeaders['X-Extend-Mage-Module-Version'],
+            $actualHeaders['X-Extend-Mage-Module-Version']
         );
         $this->assertEquals($expectedBody['webhook_id'], $actualBody['webhook_id']);
         $this->assertEquals($expectedBody['topic'], $actualBody['topic']);
