5d5e85f913b0da01fd7add24c7d951f9f41eca93
Jean-Michel Batty <jean-michel.batty@extend.com>
2024-10-28 16:40:04 -0500

    Update product-protection-simple-offer.js
    fixed so we always have a integer value

--- view/frontend/web/js/view/cart/product-protection-simple-offer.js
+++ view/frontend/web/js/view/cart/product-protection-simple-offer.js
@@ -74,7 +74,7 @@ define(['cartUtils', 'extendSdk', 'ExtendMagento', 'stringUtils'], function (
 
     const activeProductData = {
       referenceId: config[0].selectedProductSku,
-      price: parseInt(config[0].selectedProductPrice * 100).toPrecision(10),
+      price: parseInt((config[0].selectedProductPrice * 100).toPrecision(10)),
       category: config[0].productCategory,
       onAddToCart: handleAddToCartClick,
     }
